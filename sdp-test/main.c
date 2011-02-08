a210e8afc95bf671e5d1fc175d089402e5f35662
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2011-02-08 17:13:40 +0100

    youhouuuuuu

--- sdp-test/main.c
+++ sdp-test/main.c
@@ -40,6 +40,7 @@ int main()
 {
     unsigned char keyCode;
 	bdaddr_t dst;
+	const char * address = "00:10:60:A8:57:35";
 
 
     printf("// SDP test //////\n");
@@ -49,28 +50,36 @@ int main()
     
 	sdp_add_keyboard();
 	printf("----- add keyboard -----\n");
-	    
-    css = l2cap_listen(BDADDR_ANY, L2CAP_PSM_HIDP_CTRL, 0, 1);
+	
+	
+	/* Socket listening 
+    css = l2cap_listen(BDADDR_ANY, L2CAP_PSM_HIDP_CTRL, 0, 10);
 	perror("");
     printf("----- css listen--------\n");
-	
 
-	iss = l2cap_listen(BDADDR_ANY, L2CAP_PSM_HIDP_INTR, 0, 1);
+	iss = l2cap_listen(BDADDR_ANY, L2CAP_PSM_HIDP_INTR, 0, 10);
 	perror("");
 	printf("----- iss listen -------\n");
-
+    
     cs = l2cap_accept(css, NULL);
-    //cs = l2cap_connect(BDADDR_ANY, &dst, L2CAP_PSM_HIDP_CTRL);
-    //perror("");
-    printf("----- cs accept --------\n");
+    perror("");
+    printf("----- cs accept : %d --------\n", cs);
     
     is = l2cap_accept(iss, NULL);
-    //is = l2cap_connect(BDADDR_ANY, &dst, L2CAP_PSM_HIDP_INTR);
-    //perror("");
-    printf("----- is accept --------\n");
-    
+    perror("");
+    printf("----- is accept : %d --------\n", is);
+    */
 
+    str2ba( "00:10:60:A8:57:35" ,&dst);
     
+    /* Direct connection */
+    cs = l2cap_connect(BDADDR_ANY, &dst, L2CAP_PSM_HIDP_CTRL);
+    perror("");
+    printf("----- cs accept : %d --------\n", cs);
+    
+    is = l2cap_connect(BDADDR_ANY, &dst, L2CAP_PSM_HIDP_INTR);
+    perror("");
+    printf("----- is accept : %d --------\n", is);
     
     printf("\n");
 	
