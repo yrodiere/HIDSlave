2476fb38e98021299d73ae9459e869b4b4a320bc
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2011-04-04 20:19:09 +0200

    sdp : fail

--- hello-jni/jni/hello-jni.c
+++ hello-jni/jni/hello-jni.c
@@ -36,6 +36,8 @@
 #include "bluetooth/l2cap.h"
 #include "bluetooth/hci.h"
 #include "bluetooth/hci_lib.h"
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
 
 /* from original bluez hidp.h */
 #define HIDP_MINIMUM_MTU 48
@@ -78,6 +80,9 @@ static const char* FD_NAMES[] = {
 };
 
 static const char ACK[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+sdp_record_t *sdp_record;
+sdp_session_t *sdp_session;
 
 int is = 0,  iss = 0, cs = 0, css = 0;
 char logmsg[256];
@@ -140,54 +145,6 @@ void sendChar( char keycode)
     //}
 }
 
-
-/*static int l2cap_connect(bdaddr_t *src, bdaddr_t *dst, unsigned short psm)
-{
-	struct sockaddr_l2 addr;
-	struct l2cap_options opts;
-	int sk;
-	
-    sprintf ( logmsg, "HID EMUL : before error errno : %d", errno );
-    LOGV( logmsg );
-
-
-    // PF_BLUETOOTH vs. AF_BLUETOOTH ?? BTPROTO_L2CAP
-	if ((sk = socket(PF_BLUETOOTH, SOCK_SEQPACKET,  BTPROTO_L2CAP)) < 0)
-	{
-	    sprintf ( logmsg, "HID EMUL : can't open socket bluetooth : errno : %d, return : %d", errno, sk );
-        LOGE( logmsg );
-		return -1;
-    }
-    
-	memset(&addr, 0, sizeof(addr));
-	addr.l2_family  = AF_BLUETOOTH;
-	bacpy(&addr.l2_bdaddr, src);
-
-	if (bind(sk, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-		close(sk);
-		return -1;
-	}
-
-	memset(&opts, 0, sizeof(opts));
-	opts.imtu = HIDP_DEFAULT_MTU;
-	opts.omtu = HIDP_DEFAULT_MTU;
-	opts.flush_to = 0xffff;
-
-	setsockopt(sk, SOL_L2CAP, L2CAP_OPTIONS, &opts, sizeof(opts));
-
-	memset(&addr, 0, sizeof(addr));
-	addr.l2_family  = AF_BLUETOOTH;
-	bacpy(&addr.l2_bdaddr, dst);
-	addr.l2_psm = htobs(psm);
-
-	if (connect(sk, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-		close(sk);
-		return -1;
-	}
-
-	return sk;
-}*/
-
 static int l2cap_connect(bdaddr_t *src, bdaddr_t *dst, unsigned short psm)
 {
 	struct sockaddr_l2 addr;
@@ -226,6 +183,261 @@ static int l2cap_connect(bdaddr_t *src, bdaddr_t *dst, unsigned short psm)
 	return sk;
 }
 
+static void add_lang_attr(sdp_record_t *r)
+{
+	sdp_lang_attr_t base_lang;
+	sdp_list_t *langs = 0;
+
+	/* UTF-8 MIBenum (http://www.iana.org/assignments/character-sets) */
+	base_lang.code_ISO639 = (0x65 << 8) | 0x6e;
+	base_lang.encoding = 106;
+	base_lang.base_offset = SDP_PRIMARY_LANG_BASE;
+	langs = sdp_list_append(0, &base_lang);
+	sdp_set_lang_attr(r, langs);
+	sdp_list_free(langs, 0);
+}
+
+void sdp_add_keyboard()
+{
+	sdp_list_t *svclass_id, *pfseq, *apseq, *root;
+	uuid_t root_uuid, hidkb_uuid, l2cap_uuid, hidp_uuid;
+	sdp_profile_desc_t profile[1];
+	sdp_list_t *aproto, *proto[3];
+	sdp_data_t *channel, *lang_lst, *lang_lst2, *hid_spec_lst, *hid_spec_lst2;
+	int i;
+	uint8_t dtd = SDP_UINT16;
+	uint8_t dtd2 = SDP_UINT8;
+	uint8_t dtd_data = SDP_TEXT_STR8;
+	sdp_session_t *session;
+	void *dtds[2];
+	void *values[2];
+	void *dtds2[2];
+	void *values2[2];
+	int leng[2];
+	uint8_t hid_spec_type = 0x22;
+	uint16_t hid_attr_lang[] = {0x409,0x100};
+
+	static const uint8_t ctrl = L2CAP_PSM_HIDP_CTRL;
+	static const uint8_t intr = L2CAP_PSM_HIDP_INTR;
+
+	static const uint16_t hid_attr[] = {0x100,0x111,0x40,0x0d,0x01,0x01};
+        /* SDP_ATTR_HID_DEVICE_RELEASE_NUMBER = 0x100
+         * SDP_ATTR_HID_PARSER_VERSION = 0x111
+         * SDP_ATTR_HID_DEVICE_SUBCLASS = 0x40
+         * SDP_ATTR_HID_COUNTRY_CODE = 0x0d
+         * SDP_ATTR_HID_VIRTUAL_CABLE = 0x01
+         * SDP_ATTR_HID_RECONNECT_INITIATE = 0x01
+         */
+	
+	static const uint16_t hid_attr2[] = {0x0,0x01,0x100,0x1f40,0x01,0x01};
+	// taken from Apple Wireless Keyboard
+	const uint8_t hid_spec[] = { 
+		0x05, 0x01, // usage page
+		0x09, 0x06, // keyboard
+		0xa1, 0x01, // key codes
+		0x85, 0x01, // minimum
+		0x05, 0x07, // max
+		0x19, 0xe0, // logical min
+		0x29, 0xe7, // logical max
+		0x15, 0x00, // report size
+		0x25, 0x01, // report count
+		0x75, 0x01, // input data variable absolute
+		0x95, 0x08, // report count
+		0x81, 0x02, // report size
+		0x75, 0x08, 
+		0x95, 0x01, 
+		0x81, 0x01, 
+		0x75, 0x01, 
+		0x95, 0x05,
+		0x05, 0x08,
+		0x19, 0x01,
+		0x29, 0x05, 
+		0x91, 0x02,
+		0x75, 0x03,
+		0x95, 0x01,
+		0x91, 0x01,
+		0x75, 0x08,
+		0x95, 0x06,
+		0x15, 0x00,
+		0x26, 0xff,
+		0x00, 0x05,
+		0x07, 0x19,
+		0x00, 0x2a,
+		0xff, 0x00,
+		0x81, 0x00,
+		0x75, 0x01,
+		0x95, 0x01,
+		0x15, 0x00,
+		0x25, 0x01,
+		0x05, 0x0c,
+		0x09, 0xb8,
+		0x81, 0x06,
+		0x09, 0xe2,
+		0x81, 0x06,
+		0x09, 0xe9,
+		0x81, 0x02,
+		0x09, 0xea,
+		0x81, 0x02,
+		0x75, 0x01,
+		0x95, 0x04,
+		0x81, 0x01,
+		0xc0         // end tag
+	};
+
+
+	if (!sdp_session) {
+		LOGE("sdp_session invalid");
+		exit(-1);
+	}
+	session = sdp_session;
+
+	sdp_record = sdp_record_alloc();
+	if (!sdp_record) {
+		LOGE("add_keyboard sdp_record_alloc");
+		exit(-1);
+	}
+
+	memset((void*)sdp_record, 0, sizeof(sdp_record_t));
+	// handle ?????
+	sdp_record->handle = 0xffffffff;
+
+	// root uuid -> PUBLIC_BROWSE_GROUP
+	sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
+	root = sdp_list_append(0, &root_uuid);	
+	sdp_set_browse_groups(sdp_record, root);
+
+	// lang_attr : voir plus haut.
+	add_lang_attr(sdp_record);
+	
+	// Service class UUID -> HID_SVCLASS_ID
+	sdp_uuid16_create(&hidkb_uuid, HID_SVCLASS_ID);
+	svclass_id = sdp_list_append(0, &hidkb_uuid);
+	sdp_set_service_classes(sdp_record, svclass_id);
+
+	// profile
+	sdp_uuid16_create(&profile[0].uuid, HID_PROFILE_ID);
+	profile[0].version = 0x0100;
+	pfseq = sdp_list_append(0, profile);
+	sdp_set_profile_descs(sdp_record, pfseq);
+
+	// PROTO ///////////////////
+	/*
+	 * Possible liaison avec L2CAP ?
+	 * Apparement, les listes chainé sont plus que trés présente.
+	 * chaque nouveau paramètre est l'occasion d'en créer une.
+	 */
+	 
+	 //APROTO
+	     //APSEQ
+	        // PROTO[1]
+            // L2CAP uuid
+            sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
+            proto[1] = sdp_list_append(0, &l2cap_uuid);
+
+            // L2CAP channel Control
+            channel = sdp_data_alloc(SDP_UINT8, &ctrl);
+            proto[1] = sdp_list_append(proto[1], channel);
+        apseq = sdp_list_append(0, proto[1]);
+
+            // PROTO[2]
+            // HIDP uuid
+            sdp_uuid16_create(&hidp_uuid, HIDP_UUID);
+            proto[2] = sdp_list_append(0, &hidp_uuid);
+        apseq = sdp_list_append(apseq, proto[2]);
+
+    aproto = sdp_list_append(0, apseq);
+    sdp_set_access_protos(sdp_record, aproto);
+
+	// ATTR_ADD_PROTO  /////////
+	
+	// APROTO
+	    // APSEQ
+	        // PROTO[1]
+	        // L2CAP uuid
+	        proto[1] = sdp_list_append(0, &l2cap_uuid);
+	        
+	        // L2CAP channel Interruption
+	        channel = sdp_data_alloc(SDP_UINT8, &intr);
+	        proto[1] = sdp_list_append(proto[1], channel);
+	    apseq = sdp_list_append(0, proto[1]);
+
+            // PROTO[2]
+	        sdp_uuid16_create(&hidp_uuid, HIDP_UUID);
+	        proto[2] = sdp_list_append(0, &hidp_uuid);
+	    apseq = sdp_list_append(apseq, proto[2]);
+
+	aproto = sdp_list_append(0, apseq);
+	sdp_set_add_access_protos(sdp_record, aproto);
+	
+	
+	// Description
+	sdp_set_info_attr(sdp_record, "droidHID", 
+		"gravitezero & fenrhil", "original code writer : http://www.mulliner.org/bluetooth/");
+
+	for (i = 0; i < sizeof(hid_attr)/2; i++) {
+		sdp_attr_add_new(sdp_record, SDP_ATTR_HID_DEVICE_RELEASE_NUMBER+i, SDP_UINT16, &hid_attr[i]);
+		/* SDP_ATTR_HID_DEVICE_RELEASE_NUMBER = 0x100
+         * SDP_ATTR_HID_PARSER_VERSION = 0x111
+         * SDP_ATTR_HID_DEVICE_SUBCLASS = 0x40
+         * SDP_ATTR_HID_COUNTRY_CODE = 0x0d
+         * SDP_ATTR_HID_VIRTUAL_CABLE = 0x01
+         * SDP_ATTR_HID_RECONNECT_INITIATE = 0x01
+         */
+	}
+
+	dtds[0] = &dtd2;
+	values[0] = &hid_spec_type;
+	dtds[1] = &dtd_data;
+	values[1] = (uint8_t*)hid_spec;
+	leng[0] = 0;
+	leng[1] = sizeof(hid_spec);
+	hid_spec_lst = sdp_seq_alloc_with_length(dtds, values, leng, 2);
+	hid_spec_lst2 = sdp_data_alloc(SDP_SEQ8, hid_spec_lst);	
+	sdp_attr_add(sdp_record, SDP_ATTR_HID_DESCRIPTOR_LIST, hid_spec_lst2);
+	
+	for (i = 0; i < sizeof(hid_attr_lang)/2; i++) {
+		dtds2[i] = &dtd;
+		values2[i] = &hid_attr_lang[i];
+	}
+	
+	lang_lst = sdp_seq_alloc(dtds2, values2, sizeof(hid_attr_lang)/2);
+	lang_lst2 = sdp_data_alloc(SDP_SEQ8, lang_lst);	
+	sdp_attr_add(sdp_record, SDP_ATTR_HID_LANG_ID_BASE_LIST, lang_lst2);
+
+	sdp_attr_add_new(sdp_record, SDP_ATTR_HID_SDP_DISABLE, SDP_UINT16, &hid_attr2[0]);
+	for (i = 0; i < sizeof(hid_attr2)/2-1; i++) {
+		sdp_attr_add_new(sdp_record, SDP_ATTR_HID_REMOTE_WAKEUP+i, SDP_UINT16, &hid_attr2[i+1]);
+	}
+	
+	if (sdp_record_register(session, sdp_record, 0) < 0) {
+		LOGE("HID Device Service Record registration failed");
+		exit(-1);
+	}
+}
+
+void sdp_remove()
+{
+	if (sdp_record && sdp_record_unregister(sdp_session, sdp_record)) {
+		LOGE("HID Device Service Record unregistration failed");
+	}
+	
+	sdp_close(sdp_session);
+}
+
+void sdp_open()
+{
+    LOGV("Open SDP record");
+	if (!sdp_session) {
+		sdp_session = sdp_connect(BDADDR_ANY, BDADDR_LOCAL, 0);
+		LOGV("new sdp session\n");
+	}
+	if (!sdp_session) {
+		sprintf ( logmsg, "sdp_session invalid : %d", sdp_session );
+        LOGE( logmsg );
+		exit(-1);
+	}
+}
+
 
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
@@ -256,10 +468,13 @@ Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
 
     LOGV("======================= Begin ====================");
 
-    //LOGV("Add SDP record");
+    LOGV("Add SDP record");
     
-    //sdp_open();
-	//sdp_add_keyboard();
+    sdp_open();
+    
+    LOGV("Opened SDP record");
+    
+	sdp_add_keyboard();
 
     LOGV("connecting HID control channel to host");
     while(cs <= 0)
@@ -361,58 +576,3 @@ Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
 
     return (*env)->NewStringUTF(env, "Hello from HID and JNI too !");
 }
-
-/*
-================================================================================
-                         TEST CODE FROM SIMPLE SCAN
-================================================================================
-*/
-/*{
-    inquiry_info *ii = NULL;
-    int max_rsp, num_rsp;
-    int dev_id, sock, len, flags;
-    int i;
-    char addr[19] = { 0 };
-    char name[248] = { 0 };
-
-    // hci_get_route(NULL) return the id of the first available adapter
-    dev_id = hci_get_route(NULL);
-    
-    sprintf ( logmsg, "dev_id found : %d", dev_id );
-    LOGV( logmsg ); 
-
-    // hci_open_dev return a socket opened on the adapter pointed by dev_id
-    sock = hci_open_dev( dev_id );
-    if (dev_id < 0 || sock < 0) {
-        sprintf ( logmsg, " can't open sock : errno = %d", errno );
-        LOGE( logmsg ); 
-        exit(1);
-    }
-
-
-
-    len  = 8;
-    max_rsp = 255;
-
-    // flush the inquiry cache to not display out of range device (ghost devices)
-    flags = IREQ_CACHE_FLUSH;
-    ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
-    
-    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if( num_rsp < 0 )
-        LOGE("hci_inquiry");
-
-    for (i = 0; i < num_rsp; i++) {
-        ba2str(&(ii+i)->bdaddr, addr);
-        memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), 
-            name, 0) < 0)
-        strcpy(name, "[unknown]");
-        sprintf(logmsg, "%s  %s\n", addr, name);
-        LOGV(logmsg);
-    }
-
-    free( ii );
-    close( sock );
-    return (*env)->NewStringUTF(env, "Hello from HID and JNI too !");
-}*/
